fab4a517063fe0efe209a244c2ff10b64d6cda14
maopixin <pixin.mao@ourwill.com.cn>
2019-06-06 09:58:46 +0800

    change start

--- config/build.config.js
+++ config/build.config.js
@@ -1,18 +1,9 @@
 /**@build 构建*/
-const rollup = require('rollup');
 const uglify = require('rollup-plugin-uglify-es');
 const config = require('./base.config.js');
 
 config.output.file = 'dist/fereport.min.js';
+config.plugins.push(uglify());
 
-async function build() {
-    config.plugins.push(uglify());
-    const bundle = await rollup.rollup(config);
 
-    const { code, map } = await bundle.generate(config.output);
-
-    await bundle.write(config.output);
-    console.log('打包成功');
-}
-
-build();
+module.exports = config;
--- config/dev.config.js
+++ config/dev.config.js
@@ -1,31 +1,5 @@
 /**@dev 构建*/
-const rollup = require('rollup');
 const config = require('./base.config.js');
 
-// 初始化配置文件 可以根据需求扩展config
-const watcher = rollup.watch(config);
-
-watcher.on('event', event => {
-    switch (event.code) {
-        case 'START':
-            console.log('监听器正在启动（重启）');
-            break;
-        case 'BUNDLE_START':
-            console.log('构建单个文件束');
-            break;
-        case 'BUNDLE_END':
-            console.log('监听器正在启动（重启）');
-            break;
-        case 'END':
-            console.log('完成所有文件束构建');
-            break;
-        case 'ERROR':
-            console.log('构建时遇到错误');
-            break;
-        case 'FATAL':
-            console.log('遇到无可修复的错误');
-            break;
-        default:
-            break;
-    }
-});
+// 可以在上面对base配置进行修改
+module.exports = config;
